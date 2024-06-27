59a017c721f160d1685b4319a2a54a83e632b8a8
Jeremy Gu <jeremy.gu10@gmail.com>
2024-06-27 10:32:44 -0700

    Add a basic task front

---
+++ js/view/front.js
@@ -0,0 +1,53 @@
+let template_front = document.getElementById("front");
+let template_first_task = document.getElementById("first-task");
+function make_front(model) {
+	let shadow_host = document.createElement("div");
+	let shadow_root = shadow_host.attachShadow({ mode: "open" });
+	shadow_root.appendChild(template_front.content.cloneNode(true));
+	let completion_front_element = shadow_root.getElementById("completion");
+	let uncompletion_front_element = shadow_root.getElementById("uncompletion");
+	function update() {
+		while(completion_front_element.firstChild !== null) {
+			completion_front_element.removeChild(completion_front_element.firstChild);
+		}
+		while(uncompletion_front_element.firstChild !== null) {
+			uncompletion_front_element.removeChild(uncompletion_front_element.firstChild);
+		}
+		if(model.repository.data.completion_front.length !== 0) {
+			let first_task = model.repository.data.completion_front[0];
+			let first_task_element = document.createElement("li");
+			completion_front_element.appendChild(first_task_element);
+			let first_task_shadow_host = document.createElement("div");
+			first_task_element.appendChild(first_task_shadow_host);
+			let first_task_shadow_root = first_task_shadow_host.attachShadow({ mode: "open" });
+			first_task_shadow_root.appendChild(template_first_task.content.cloneNode(true));
+			let first_task_title_element = first_task_shadow_root.getElementById("title");
+			let first_task_description_element = first_task_shadow_root.getElementById("description");
+			let first_task_complete_element = first_task_shadow_root.getElementById("complete");
+			let first_task_cycle_element = first_task_shadow_root.getElementById("cycle");
+			first_task_title_element.textContent = first_task.title;
+			first_task_description_element.textContent = first_task.description;
+			first_task_complete_element.onclick = function(event) {
+				model.complete();
+				update();
+			};
+			first_task_cycle_element.onclick = function(event) {
+				model.cycle();
+				update();
+			};
+			for(let index = 1; index < model.repository.data.completion_front.length; index++) {
+				let task = model.repository.data.completion_front[index];
+				let task_element = document.createElement("li");
+				completion_front_element.appendChild(task_element);
+				task_element.textContent = task.title;
+			}
+		}
+		for(let task of model.repository.data.uncompletion_front) {
+			let task_element = document.createElement("li");
+			uncompletion_front_element.appendChild(task_element);
+			task_element.textContent = task.title;
+		}
+	}
+	update();
+	return shadow_host;
+}
